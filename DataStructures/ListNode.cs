a9129b600655db2f9c2408d9c706f550f44802f9
FW960 <artuninp@gmail.com>
2022-07-30 08:27:42 +0800

    Initial commit

---
+++ DataStructures/ListNode.cs
@@ -0,0 +1,121 @@
+﻿namespace Test;
+
+public class ListNode<T>
+{
+    public readonly T Value;
+
+    public readonly ListNode<T>? Next;
+
+    #region Constructors
+
+    public ListNode(T value)
+    {
+        Value = value;
+    }
+
+    private ListNode(T value, ListNode<T> next, ListNode<T> toAppend, int depth)
+    {
+        depth--;
+
+        Value = value;
+
+        if (depth > 0)
+        {
+            Next = new ListNode<T>(next.Value, next.Next, toAppend, depth);
+            return;
+        }
+        else if (depth <= 0)
+        {
+            if (toAppend.Next != null)
+                Next = new ListNode<T>(toAppend.Value, toAppend.Next);
+            else
+                Next = new ListNode<T>(toAppend.Value);
+        }
+    }
+
+    private ListNode(T value, ListNode<T> next)
+    {
+        Value = value;
+
+        if (next != null)
+            Next = new ListNode<T>(next.Value, next.Next);
+    }
+    
+
+    private ListNode(ListNode<T> current, ListNode<T> next, int position, int count, T value)
+    {
+        if (position == count)
+            Value = value;
+        else
+            Value = current.Value;
+
+        count++;
+
+        if (next == null)
+            return;
+        else
+            Next = new ListNode<T>(current.Next, next.Next, position, count, value);
+    }
+
+    private ListNode(T value, ListNode<T> next, ListNode<T> toChange, int count, int position)
+    {
+        count++;
+
+        Value = value;
+
+        if (next == null)
+            return;
+
+        if (count + 1 == position)
+        {
+            if (toChange.Next != null)
+                Next = new ListNode<T>(toChange.Value, toChange.Next);
+            else
+                Next = new ListNode<T>(toChange.Value);
+
+            return;
+        }
+
+        Next = new ListNode<T>(next.Value, next.Next, toChange, count, position);
+    }
+
+    #endregion
+
+    public ListNode<T> AppendNode(ListNode<T> last) => new ListNode<T>(Value, Next, last, CalculateDepth());
+
+    public ListNode<T> AppendValue(T value) => new ListNode<T>(Value, Next, new ListNode<T>(value), CalculateDepth());
+
+    public ListNode<T> ChangeNode(int position, ListNode<T> node)
+    {
+        if (position <= 0)
+            throw new Exception("Linked list positions begins from 1");
+        
+        if (position == 1)
+            return new ListNode<T>(node.Value, node.Next);
+
+        return new ListNode<T>(Value, Next, node, 0, position);
+    }
+
+    public ListNode<T> ChangeValue(int position, T value)
+    {
+        if (position <= 0)
+            throw new Exception("Linked list positions begins from 1");
+        
+        return new ListNode<T>(this, Next, position, 1, value);
+    }
+
+    public int CalculateDepth()
+    {
+        int count = 1;
+
+        var node = Next;
+
+        while (node != null)
+        {
+            node = node.Next;
+            count++;
+        }
+
+        return count;
+    }
+}
---
+++ DataStructures/Tree.cs
@@ -0,0 +1,100 @@
+﻿namespace Test;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class Tree
+{
+    public int? Value { get; private set; }
+    public Tree? Parent { get; private set; }
+    public Tree? LeftBranch { get; private set; }
+    public Tree? RightBranch { get; private set; }
+
+    public Tree(int value)
+    {
+        Value = value;
+        
+    }
+
+    public void BinaryEnter(int value)
+    {
+        if (Value == null)
+        {
+            Value = value;
+            return;
+        }
+
+        if (value > Value && RightBranch != null)
+        {
+            RightBranch.BinaryEnter(value);
+        }
+        else if (value > Value)
+        {
+            RightBranch = new Tree(value);
+            RightBranch.Parent = this;
+            RightBranch.BinaryEnter(value);
+        }
+
+        if (value < Value && LeftBranch != null)
+        {
+            LeftBranch.BinaryEnter(value);
+        }
+        else if (value < Value)
+        {
+            LeftBranch = new Tree(value);
+            LeftBranch.Parent = this;
+            LeftBranch.BinaryEnter(value);
+        }
+    }
+
+    public List<Tree> BreadthFirstSearch()
+    {
+        var queue = new Queue<Tree>();
+        queue.Enqueue(this);
+
+        return BreadthFirstSearch(queue, new List<Tree>());
+    }
+
+    private List<Tree> BreadthFirstSearch(Queue<Tree> queue, List<Tree> nodes)
+    {
+        if (queue.Count == 0)
+            return nodes;
+
+        if (queue.First().LeftBranch != null)
+            queue.Enqueue(queue.First().LeftBranch);
+
+        if (queue.First().RightBranch != null)
+            queue.Enqueue(queue.First().RightBranch);
+
+        nodes.Add(queue.Dequeue());
+
+        return BreadthFirstSearch(queue, nodes);
+    }
+
+    public List<Tree> DepthFirstSearch()
+    {
+        var stack = new Stack<Tree>();
+        stack.Push(this);
+
+        return DepthFirstSearch(stack, new List<Tree>());
+    }
+
+    private List<Tree> DepthFirstSearch(Stack<Tree> stack, List<Tree> nodes)
+    {
+        if (stack.First().LeftBranch != null)
+        {
+            stack.Push(stack.First().LeftBranch);
+            DepthFirstSearch(stack, nodes);
+        }
+
+        if (stack.First().RightBranch != null)
+        {
+            stack.Push(stack.First().RightBranch);
+            DepthFirstSearch(stack, nodes);
+        }
+
+        nodes.Add(stack.Pop());
+
+        return nodes;
+    }
+}
